a2359e6fb37a34faae894cc4095c39ff34462d1b
Monserrat Palma <Mokantg@users.noreply.github.com>
2023-05-24 15:34:53 -0400

    reto 6 24-05-23

--- 05-ciclos_y_bucles.py
+++ 05-ciclos_y_bucles.py
@@ -84,3 +84,4 @@ print('>> Tercer For')
 for i in range(1, 11):
     print(i)
 print('\n')
+
---
+++ retos/reto-06.py
@@ -0,0 +1,8 @@
+n = 10
+t = 0
+
+while n<=30:
+    print(n)
+    t = t+n
+    n += 2
+print(f'La suma de todos estos números es: {t}')
